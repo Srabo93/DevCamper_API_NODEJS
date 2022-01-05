82d7fd2a0f971e441bf9577d0e96702587f3c7e8
Srabo93 <A.S-dev@web.de>
2022-01-05 14:41:15 +0100

    added mongoose, mongoDB connection

--- server.js
+++ server.js
@@ -4,14 +4,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-/**
- * ROUTE FILES
- */
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 /**
  * LOAD ENV VARS
  */
 dotenv.config({ path: "./config/config.env" });
+/**
+ * CONNECT TO DATABASE
+ */
+connectDB();
+/**
+ * ROUTE FILES
+ */
+const bootcamps = require("./routes/bootcamps");
 /**
  * INITIALIZE APP
  */
@@ -29,7 +34,16 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+/**
+ * HANDLE UNHANDLED PROMISED REJECTIONS
+ */
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close Server & Exit proces
+  server.close(() => process.exit(1));
+});
