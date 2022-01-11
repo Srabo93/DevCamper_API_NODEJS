cb672bafabe19281a213c45886dc35b1d8cd9231
Srabo93 <A.S-dev@web.de>
2022-01-11 10:36:24 +0100

    added Slugify, implemented as pre processor

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const slugify = require("slugify");
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -96,5 +96,11 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+/*CREATE BOOTCAMP SLUG FROM THE NAME */
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
