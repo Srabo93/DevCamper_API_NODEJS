62e5c0895df03385d851442371958d4ce72a6ea9
Srabo93 <A.S-dev@web.de>
2022-01-12 11:58:05 +0100

    -seeder script to import from local folder / delete

---
+++ seeder.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+/*LOAD ENV VARS */
+dotenv.config({ path: "./config/config.env" });
+
+/*LOAD MODELS */
+const Bootcamp = require("./models/Bootcamp");
+
+/*CONNECT TO DB */
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+/*READ JSON FILES  */
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+/*IMPORT INTO DB */
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/*DELETE DATA */
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("Data destroyed...".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
