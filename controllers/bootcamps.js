99d2e4ae19cf1e444403a6432335370dc8b83581
Srabo93 <A.S-dev@web.de>
2022-01-18 15:45:49 +0100

    population of courses in bootcamps, build virtual between Bootcamp and Course, added cascade Deletion

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,7 +24,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
   //Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
   //Select Fields
   if (req.query.select) {
     const fields = req.query.select.split(",").join(" ");
@@ -119,12 +119,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
  * @access Private
  */
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with ID of ${req.params.id}`, 404)
     );
   }
+  bootcamp.remove();
   res.status(200).json({ success: true, data: {} });
 });
 
