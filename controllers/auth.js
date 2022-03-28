4c72c66b34018798e246ccfcb3e747c010aa449f
Srabo93 <A.S-dev@web.de>
2022-03-28 21:20:56 +0200

    added user logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,6 +50,21 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 /**
+ * Logout user
+ * @route GET /api/v1/auth/logout
+ * @access Private
+ */
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 100),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+/**
  * Get current logged in user
  * @route POST /api/v1/auth/me
  * @access Private
