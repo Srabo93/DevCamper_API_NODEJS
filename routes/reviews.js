7cc9956fd79f776641b6f559ae6bec1abff9a36a
Srabo93 <A.S-dev@web.de>
2022-03-28 19:57:19 +0200

    added update and delte review

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ const {
   getReviews,
   getReview,
   addReview,
+  updateReview,
+  deleteReview,
 } = require("../controllers/reviews.js");
 const router = express.Router({ mergeParams: true });
 const Review = require("../models/Review");
@@ -17,6 +19,10 @@ router
     getReviews
   )
   .post(protect, authorize("user", "admin"), addReview);
-router.route("/:id").get(getReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
